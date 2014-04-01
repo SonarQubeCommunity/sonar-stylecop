9a776d4c0202be4c2a60860d5d77d72a51532426
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-04-01 17:19:21 +0200

    SONARPLUGINS-3607 Add the StyleCop settings writer

---
+++ src/main/java/org/sonar/plugins/stylecop/StyleCopSettingsWriter.java
@@ -0,0 +1,99 @@
+/*
+ * SonarQube StyleCop Plugin
+ * Copyright (C) 2014 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.stylecop;
+
+import com.google.common.base.Charsets;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
+import com.google.common.io.Files;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+public class StyleCopSettingsWriter {
+
+  public void write(List<String> ruleConfigKeys, File file) {
+    StringBuilder sb = new StringBuilder();
+
+    appendLine(sb, "<StyleCopSettings Version=\"105\">");
+    appendLine(sb, "  <Analyzers>");
+
+    for (String ruleNamespace : ruleNamespaces(ruleConfigKeys)) {
+      appendLine(sb, "    <Analyzer AnalyzerId=\"" + ruleNamespace + "\">");
+      appendLine(sb, "      <Rules>");
+      for (String ruleKey : ruleKeys(ruleNamespace, ruleConfigKeys)) {
+        appendLine(sb, "        <Rule Name=\"" + ruleKey + "\">");
+        appendLine(sb, "          <RuleSettings>");
+        appendLine(sb, "            <BooleanProperty Name=\"Enabled\">True</BooleanProperty>");
+        appendLine(sb, "          </RuleSettings>");
+        appendLine(sb, "        </Rule>");
+      }
+      appendLine(sb, "      </Rules>");
+      appendLine(sb, "    </Analyzer>");
+    }
+
+    appendLine(sb, "  </Analyzers>");
+    appendLine(sb, "</StyleCopSettings>");
+
+    try {
+      Files.write(sb.toString().getBytes(Charsets.UTF_8), file);
+    } catch (IOException e) {
+      throw Throwables.propagate(e);
+    }
+  }
+
+  private static List<String> ruleNamespaces(List<String> ruleConfigKeys) {
+    ImmutableList.Builder<String> builder = ImmutableList.builder();
+    Set<String> alreadyAddedNamespaces = Sets.newHashSet();
+
+    for (String ruleConfigKey : ruleConfigKeys) {
+      String ruleNamespace = ruleConfigKey.substring(0, ruleConfigKey.indexOf('#'));
+      if (!alreadyAddedNamespaces.contains(ruleNamespace)) {
+        builder.add(ruleNamespace);
+        alreadyAddedNamespaces.add(ruleNamespace);
+      }
+    }
+
+    return builder.build();
+  }
+
+  private static List<String> ruleKeys(String ruleNamespace, List<String> ruleConfigKeys) {
+    ImmutableList.Builder<String> builder = ImmutableList.builder();
+
+    for (String ruleConfigKey : ruleConfigKeys) {
+      if (ruleConfigKey.startsWith(ruleNamespace)) {
+        String ruleKey = ruleConfigKey.substring(ruleNamespace.length() + 1);
+        builder.add(ruleKey);
+      }
+    }
+
+    return builder.build();
+  }
+
+  private static void appendLine(StringBuilder sb, String s) {
+    sb.append(s);
+    sb.append(IOUtils.LINE_SEPARATOR);
+  }
+
+}
---
+++ src/test/java/org/sonar/plugins/stylecop/StyleCopSettingsWriterTest.java
@@ -0,0 +1,85 @@
+/*
+ * SonarQube StyleCop Plugin
+ * Copyright (C) 2014 SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.stylecop;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Files;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class StyleCopSettingsWriterTest {
+
+  @Rule
+  public TemporaryFolder tmp = new TemporaryFolder();
+
+  @Test
+  public void test() throws Exception {
+    File file1 = tmp.newFile();
+
+    new StyleCopSettingsWriter().write(ImmutableList.of("foo#A", "foo#B", "bar#C"), file1);
+    String contents1 = Files.toString(file1, Charsets.UTF_8);
+
+    assertThat(contents1.replace("\r", "").replace("\n", ""))
+      .isEqualTo(
+        "<StyleCopSettings Version=\"105\">"
+          + "  <Analyzers>"
+          + "    <Analyzer AnalyzerId=\"foo\">"
+          + "      <Rules>"
+          + "        <Rule Name=\"A\">"
+          + "          <RuleSettings>"
+          + "            <BooleanProperty Name=\"Enabled\">True</BooleanProperty>"
+          + "          </RuleSettings>"
+          + "        </Rule>"
+          + "        <Rule Name=\"B\">"
+          + "          <RuleSettings>"
+          + "            <BooleanProperty Name=\"Enabled\">True</BooleanProperty>"
+          + "          </RuleSettings>"
+          + "        </Rule>"
+          + "      </Rules>"
+          + "    </Analyzer>"
+          + "    <Analyzer AnalyzerId=\"bar\">"
+          + "      <Rules>"
+          + "        <Rule Name=\"C\">"
+          + "          <RuleSettings>"
+          + "            <BooleanProperty Name=\"Enabled\">True</BooleanProperty>"
+          + "          </RuleSettings>"
+          + "        </Rule>"
+          + "      </Rules>"
+          + "    </Analyzer>"
+          + "  </Analyzers>"
+          + "</StyleCopSettings>");
+
+    File file2 = tmp.newFile();
+    new StyleCopSettingsWriter().write(ImmutableList.of("baz#SomeRuleKey"), file2);
+    String contents2 = Files.toString(file2, Charsets.UTF_8);
+
+    assertThat(contents2)
+      .contains("baz")
+      .contains("SomeRuleKey")
+      .doesNotContain("foo");
+  }
+
+}
